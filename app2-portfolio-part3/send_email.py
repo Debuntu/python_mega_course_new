286082d8fe940a551025ec55c128423143eaa10c
Debuntu <49510414+Debuntu@users.noreply.github.com>
2025-01-22 23:04:01 -0600

    adding comments on send_mail.py

--- app2-portfolio-part3/send_email.py
+++ app2-portfolio-part3/send_email.py
@@ -3,11 +3,12 @@ import smtplib, ssl
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
-
+    #created an app password using my gmail so we dont want to expose our actual gmail password.
     username = "deba25taluk@gmail.com"
     password = "tsma xecf towc wynp"
 
     my_context = ssl.create_default_context()
+    #receiver could be any email address, I am just using mine for experimenting. 
     receiver = "deba25taluk@gmail.com"
 
 
